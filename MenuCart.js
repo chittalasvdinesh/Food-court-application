3c87c34431bdbbf8bf5721189914b03711f027c5
chittalasvdinesh <86600533+chittalasvdinesh@users.noreply.github.com>
2021-12-21 18:04:30 +0530

    Add files via upload

---
+++ MenuCart.js
@@ -0,0 +1,574 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Nellore Biryani',
+    category : 'biryani',
+    rating : 4.5,
+    price: 200,
+    img: 'https://tse3.mm.bing.net/th?id=OIP.k1g15jRTR974wZsbmoE1_QHaE7&pid=Api&P=0&w=248&h=166',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Hyderabadi Biryani',
+    category : 'biryani',
+    rating : 4.8,
+    price: 225,
+    img: 'https://tse1.mm.bing.net/th?id=OIP.MnjkS72gcWWDOTfoiUB3tAHaE8&pid=Api&P=0&w=286&h=192',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Egg Biryani',
+    category : 'biryani',
+    rating : 4.2,
+    price: 180,
+    img: 'https://tse2.mm.bing.net/th?id=OIP.qwfYlJtEvDdC9dgSBQbPMQHaE8&pid=Api&P=0&w=227&h=152',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Goan Fish Biryani',
+    category : 'biryani',
+    rating : 4.0,
+    price: 210,
+    img: 'https://tse3.mm.bing.net/th?id=OIP.yUadp7sMgolsJ4MlE7AkVwHaG5&pid=Api&P=0&w=168&h=157',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Mutton Biryani',
+    category : 'biryani',
+    rating : 4.0,
+    price: 250,
+    img: 'https://tse4.mm.bing.net/th?id=OIP.wvbuAXub4mdBsPERBOaROwHaE8&pid=Api&P=0&w=238&h=160',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'natukodi palaov',
+    category : 'biryani',
+    rating : 5.0,
+    price: 240,
+    img: 'https://tse4.mm.bing.net/th?id=OIP.4weKh-FvWFMwgJsCq3ZB5wHaEK&pid=Api&P=0&w=319&h=180',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Prawns biryani',
+    category : 'biryani',
+    rating : 4.7,
+    price: 225,
+    img: 'https://tse2.mm.bing.net/th?id=OIP.vc4Bxr1-W4yOktf29DYlhgHaEK&pid=Api&P=0&w=290&h=164',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Pot biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 270,
+    img: 'https://tse3.mm.bing.net/th?id=OIP.CQaPLDmvcxH-uomiFPoxyQHaD2&pid=Api&P=0&w=340&h=178',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Mughlai Biryani',
+    category : 'biryani',
+    rating : 4.2,
+    price: 195,
+    img: 'https://tse1.mm.bing.net/th?id=OIP.bvCwUi97l3WymycP-P6DcQHaD4&pid=Api&P=0&w=298&h=157',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Vegetable Korma',
+    category : 'vegetable',
+    rating : 4.5,
+    price: 90,
+    img: 'https://tse2.mm.bing.net/th?id=OIP.8I79U5KCC9rlZymJlPa9vwHaGe&pid=Api&P=0&w=196&h=172',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Veg fried rice',
+    category : 'vegetable',
+    rating : 4.8,
+    price: 100,
+    img: 'https://tse4.mm.bing.net/th?id=OIP.5f5bW_Aec0FC0D9anmV-SQHaEo&pid=Api&P=0&w=256&h=161',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Veg Biryani',
+    category : 'vegetable',
+    rating : 4.7,
+    price: 110,
+    img: 'https://tse1.mm.bing.net/th?id=OIP.-FWmQLMeqUiMfOF2Um8qegHaE8&pid=Api&P=0&w=226&h=151',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Veg Curry',
+    category : 'vegetable',
+    rating : 4.9,
+    price: 75,
+    img: 'https://tse3.mm.bing.net/th?id=OIP.v6mVZuhYptY_lYMxZmL0AwHaHa&pid=Api&P=0&w=300&h=300',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Sambar rice',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 85,
+    img: 'https://tse3.mm.bing.net/th?id=OIP.Wi38xb3Pba-qGuTc0XFY4gHaEt&pid=Api&P=0&w=258&h=165',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Veg Masala',
+    category : 'vegetable',
+    rating : 4.5,
+    price: 90,
+    img: 'https://tse2.mm.bing.net/th?id=OIP.IUF92y2QneXu1inGpshJYAHaHa&pid=Api&P=0&w=300&h=300',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Veg Pakoda',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 50,
+    img: 'https://tse1.mm.bing.net/th?id=OIP.0FW3whxnzLvMlvxYyYPE7QHaE8&pid=Api&P=0&w=241&h=161',
+    quantity: 1
+},
+
+
+{
+    id: 17,
+    name: 'Butter Masala Dosa',
+    category : 'south indian',
+    rating : 4.7,
+    price: 65,
+    img: 'https://tse1.mm.bing.net/th?id=OIP.LVMdg80ForHft-uSf0Y16AHaFj&pid=Api&P=0&w=218&h=164',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Idli',
+    category : 'south indian',
+    rating : 5.0,
+    price: 30,
+    img: 'https://tse2.mm.bing.net/th?id=OIP.wjJb18dLp1PjJ8PRXNKdSAHaFz&pid=Api&P=0&w=223&h=176',
+    quantity: 1
+},
+{
+    id: 19,
+    name: 'panner Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 55,
+    img: 'https://tse2.mm.bing.net/th?id=OIP.HjnDhIBQj7qDZMHKGIlRrAHaE8&pid=Api&P=0&w=252&h=169',
+    quantity: 1
+},
+{
+    id: 20,
+    name: 'Mysore Bonda',
+    category : 'south indian',
+    rating : 4.5,
+    price: 35,
+    img: 'https://tse1.mm.bing.net/th?id=OIP.09jhPJ-0KKMJ6mecvF2V6QHaFz&pid=Api&P=0&w=232&h=183',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Rava dosa',
+    category : 'south indian',
+    rating : 4.4,
+    price: 45,
+    img: 'https://tse3.mm.bing.net/th?id=OIP.vJ2DE5T577ny1NcosqUE8QHaE8&pid=Api&P=0&w=268&h=179',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Plain Dosa',
+    category : 'south indian',
+    rating : 4.2,
+    price: 40,
+    img: 'https://tse4.mm.bing.net/th?id=OIP.tlcWGrGvBWmKwTXhHxUV9QHaE8&pid=Api&P=0&w=228&h=153',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Rava Uttapam',
+    category : 'south indian',
+    rating : 4.6,
+    price: 50,
+    img: 'https://tse3.mm.bing.net/th?id=OIP.o3qG5vdmpl9WqIty9_KRyAHaFA&pid=Api&P=0&w=269&h=183',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Sambhar Idly',
+    category : 'south indian',
+    rating : 5.0,
+    price: 45,
+    img: 'https://tse2.mm.bing.net/th?id=OIP.9Y5etkfVm0qgr8sBMWIjgQHaFv&pid=Api&P=0&w=208&h=163',
+    quantity: 1
+},
+]
+
+
+
+
+function showItems(){
+    var biryani= document.getElementById('biryani');
+    var vegetable=  document.getElementById('vegetable');
+    var southIndian=  document.getElementById('south-indian');
+
+    /*filtering the items using category key in food Items array*/
+
+    const biryaniSection= foodItem.filter((item)=>item.category=='biryani');
+    const vegetableSection= foodItem.filter((item)=>item.category=='vegetable');
+    const southIndianSection= foodItem.filter((item)=>item.category=='south indian');
+   
+    /*adding card items of specific section*/
+    biryaniSection.map(item=>{
+        
+        var itemCard= document.createElement('div');
+        itemCard.setAttribute('id','item-card')
+
+        var cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        var star= document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        var heart= document.createElement('i');
+        heart.setAttribute('class','fa fa-heart-o add-to-cart');
+        heart.setAttribute('id',item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img= document.createElement('img');
+        img.src=item.img;
+
+        var itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText= item.name;
+
+        var itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : ₹ ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+        biryani.appendChild(itemCard);
+        
+    })
+
+/*adding card items of specific section*/
+
+
+    vegetableSection.map(item=>{
+        var itemCard= document.createElement('div');
+        itemCard.setAttribute('id','item-card')
+
+        var cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        var star= document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        var heart= document.createElement('i');
+        heart.setAttribute('class','fa fa-heart-o add-to-cart');
+        heart.setAttribute('id',item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img= document.createElement('img');
+        img.src=item.img;
+
+        var itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText= item.name;
+
+        var itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : ₹ ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        
+        vegetable.appendChild(itemCard)
+    
+    })
+
+    
+    southIndianSection.map(item=>{
+        var itemCard= document.createElement('div');
+        itemCard.setAttribute('id','item-card')
+
+        var cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        var star= document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        var heart= document.createElement('i');
+        heart.setAttribute('class','fa fa-heart-o add-to-cart');
+        heart.setAttribute('id',item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img= document.createElement('img');
+        img.src=item.img;
+
+        var itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText= item.name;
+
+        var itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : ₹ ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        southIndian.appendChild(itemCard)
+
+    })
+}
+showItems();
+
+
+const vegData= [...new Map(foodItem.map(item=> [item['category'],item])).values()];
+
+
+function selectTaste(){
+    var categoryList= document.getElementById('category-list');
+
+    vegData.map(item=>{
+        console.log(item)
+        var listCard= document.createElement('div');
+        listCard.setAttribute('class','list-card');
+    
+        var listImg= document.createElement('img');
+        listImg.src= item.img;
+    
+        var listName= document.createElement('a');
+        listName.setAttribute('class','list-name');
+        listName.innerText= item.category;
+        listName.setAttribute('href','#'+item.category)
+    
+        listCard.appendChild(listImg);
+        listCard.appendChild(listName);
+
+        var cloneListCard= listCard.cloneNode(true);
+        categoryList.appendChild(listCard);
+        document.querySelector('.category-header').appendChild(cloneListCard)
+    })
+}
+selectTaste();
+
+
+document.querySelectorAll('.add-to-cart').forEach(item=>{
+    item.addEventListener('click',addToCart)
+})
+
+var cartData= [];
+function addToCart(){
+    
+    var itemToAdd= this.parentNode.nextSibling.nextSibling.innerText;
+    var itemObj= foodItem.find(element=>element.name==itemToAdd);
+
+    var index= cartData.indexOf(itemObj);
+    if(index=== -1){
+        document.getElementById(itemObj.id).classList.add('toggle-heart');
+        cartData= [...cartData,itemObj];
+    }
+    else if(index > -1){
+        alert("Added to cart!");
+    }
+    
+    document.getElementById('cart-plus').innerText=
+    ' ' + cartData.length + ' Items';
+    document.getElementById('m-cart-plus').innerText=
+    ' ' + cartData.length;
+    totalBill();
+    cartList();
+}
+
+/* final cart list added in the form of table which contains item name and price with number of quantity*/
+function cartList(){
+    var tableBody=  document.getElementById('table-body');
+    tableBody.innerHTML= '';
+
+    cartData.map(item=> {
+        var tableRow= document.createElement('tr');
+        
+        var rowData1= document.createElement('td');
+        var img= document.createElement('img');
+        img.src= item.img;
+        rowData1.appendChild(img);
+    
+        var rowData2= document.createElement('td');
+        rowData2.innerText= item.name;
+        
+        var rowData3= document.createElement('td');
+        var btn1= document.createElement('button');
+        btn1.setAttribute('class','decrease-item');
+        btn1.innerText= '-';
+        var span= document.createElement('span');
+        span.innerText= item.quantity;
+        var btn2= document.createElement('button');
+        btn2.setAttribute('class','increase-item');
+        btn2.innerText= '+';
+        
+        rowData3.appendChild(btn1);
+        rowData3.appendChild(span);
+        rowData3.appendChild(btn2);
+    
+        var rowData4= document.createElement('td');
+        rowData4.innerText= item.price;
+    
+        tableRow.appendChild(rowData1);
+        tableRow.appendChild(rowData2);
+        tableRow.appendChild(rowData3);
+        tableRow.appendChild(rowData4);
+    
+        tableBody.appendChild(tableRow);
+    })
+    document.querySelectorAll('.increase-item').forEach(item=>{
+        item.addEventListener('click',incrementItem)
+    })
+
+    document.querySelectorAll('.decrease-item').forEach(item=>{
+        item.addEventListener('click',decrementItem)
+    })
+}
+
+
+function incrementItem(){
+    let itemToInc= this.parentNode.previousSibling.innerText;
+    console.log(itemToInc)
+    var incObj= cartData.find(element=>element.name==itemToInc);
+    incObj.quantity+=1;
+    
+    currPrice= (incObj.price*incObj.quantity - incObj.price*(incObj.quantity-1))/(incObj.quantity-1);
+    incObj.price= currPrice*incObj.quantity;
+    totalAmount()
+    cartItems();
+}
+
+var currPrice= 0;
+function decrementItem(){
+    let itemToInc= this.parentNode.previousSibling.innerText;
+    let decObj= cartData.find(element=>element.name==itemToInc);
+    let ind= cartData.indexOf(decObj);
+    if(decObj.quantity >1){
+        currPrice= (decObj.price*decObj.quantity - decObj.price*(decObj.quantity-1))/(decObj.quantity);
+        decObj.quantity-= 1;
+        decObj.price= currPrice*decObj.quantity;
+    }
+    else{
+        document.getElementById(decObj.id).classList.remove('toggle-heart')
+        cartData.splice(ind,1);
+        document.getElementById('cart-plus').innerText= ' ' + cartData.length + ' Items';
+        document.getElementById('m-cart-plus').innerText= ' ' + cartData.length;
+        if(cartData.length < 1 && flag){
+            document.getElementById('food-items').classList.toggle('food-items');
+            document.getElementById('category-list').classList.toggle('food-items');
+            document.getElementById('m-cart-plus').classList.toggle('m-cart-toggle')
+            document.getElementById('cart-page').classList.toggle('cart-toggle');
+            document.getElementById('category-header').classList.toggle('toggle-category');
+            document.getElementById('checkout').classList.toggle('cart-toggle');
+            flag= false;
+            alert("Currently no item in cart!");
+            console.log(flag)
+        }
+    }
+    totalAmount()
+    cartItems()
+}
+
+function totalBill(){
+    var sum=0;
+    cartData.map(item=>{
+        sum=sum+ item.price;
+    })
+    document.getElementById('total-item').innerText= 'Total Item : ' + cartData.length;
+    document.getElementById('total-price').innerText= 'Total Price : ₹ ' + sum;
+    document.getElementById('m-total-amount').innerText= 'Total Price : ₹ ' + sum;
+}
+
+document.getElementById('cart-plus').addEventListener('click',cartToggle);
+document.getElementById('m-cart-plus').addEventListener('click',cartToggle);
+
+var flag= false;
+function cartToggle(){
+    if(cartData.length > 0){
+        document.getElementById('food-items').classList.toggle('food-items');
+        document.getElementById('category-list').classList.toggle('food-items');
+        document.getElementById('category-header').classList.toggle('toggle-category');
+        document.getElementById('m-cart-plus').classList.toggle('m-cart-toggle')
+        document.getElementById('cart-page').classList.toggle('cart-toggle');
+        document.getElementById('checkout').classList.toggle('cart-toggle');
+        flag= true;
+        console.log(flag)
+    }
+    else{
+        alert("Currently there is no item in cart!");
+    }
+}
+
+
+
+
+
+function addEvents(){
+    document.querySelectorAll('.add-to-cart').forEach(item=>{
+        item.addEventListener('click',addToCart)
+    });
+    document.querySelectorAll('.increase-item').forEach(item=>{
+        item.addEventListener('click',incrementItem)
+    })
+
+    document.querySelectorAll('.decrease-item').forEach(item=>{
+        item.addEventListener('click',decrementItem)
+    })
+}
+
+document.getElementById('add-address').addEventListener('click',addLocation);
+
+document.getElementById('m-add-address').addEventListener('click',addLocation);
+
+function addLocation(){
+    var location= prompt('Please Enter your location','');
+    if(location){
+        document.getElementById('add-address').innerText= ' ' + address;
+    }
+    else{
+        alert("Location not added")
+    }
+	}
